424d57e5621c87b81a9c245e92a3883af5c49fce
Tuhsara Vepakomma <tvepakom@uncc.edu>
2019-02-24 18:21:31 -0500

    First commit

---
+++ Project Credit Card/Project Credit Card/code/creditcard/App/models.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+# class trees(models.Model):
+#     Logistic_Regression = models.IntegerField()
+#     Decison_Tree = models.IntegerField()
+#     K_nearestNeighbour = models.IntegerField()
+#     size_of_x_train = models.IntegerField()
+#     size_of_y_train = models.IntegerField()
